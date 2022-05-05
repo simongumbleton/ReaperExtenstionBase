4bef7326770d64f144707d762e440e0223e686f7
simongumbleton <simongumbleton@gmail.com>
2022-05-05 22:48:41 +0100

    update juce headers

--- ReaperExtensionBase/include/JUCE/JuceHeader.h
+++ ReaperExtensionBase/include/JUCE/JuceHeader.h
@@ -12,20 +12,20 @@
 
 #pragma once
 
-#include "AppConfig.h"
 
 #include <juce_core/juce_core.h>
 #include <juce_data_structures/juce_data_structures.h>
 #include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
 #include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
 
 
 #if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
  /** If you've hit this error then the version of the Projucer that was used to generate this project is
      older than the version of the JUCE modules being included. To fix this error, re-save your project
      using the latest version of the Projucer or, if you aren't using the Projucer to manage your project,
-     remove the JUCE_PROJUCER_VERSION define from the AppConfig.h file.
+     remove the JUCE_PROJUCER_VERSION define.
  */
  #error "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
 #endif
@@ -39,7 +39,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "reaper_wwise_csg";
+    const char* const  projectName    = "JuceLib";
     const char* const  companyName    = "";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
